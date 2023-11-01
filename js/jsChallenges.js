3404372808110164fa48792d5474144f2e8f3402
Justin Fischer <jfischer@adadapted.com>
2023-11-01 09:54:20 -0400

    init commit

---
+++ js/jsChallenges.js
@@ -0,0 +1,34 @@
+function challenge1(){
+    if (document.querySelector('#emailDiv').style.display == "block"){
+        document.querySelector('#emailDiv').style.display = "none";
+    }
+
+    else{
+        document.querySelector('#emailDiv').style.display = "block";
+    }
+
+}
+
+function  processSameAddress(){
+    if (document.querySelector('#home').readOnly == true){
+        document.querySelector('#home').readOnly = false;
+    }
+    else{
+        document.querySelector('#home').value = document.querySelector('#bill').value;
+        document.querySelector('#home').readOnly = true;
+    }
+
+}
+
+function challenge3(){
+    if (document.querySelector("input[name='standing']:checked") == null){
+        return false;
+    }
+    if (document.querySelector("input[name='gradDate']:checked") == null){
+        return false;
+    }
+    return true;
+}
+
+
+
